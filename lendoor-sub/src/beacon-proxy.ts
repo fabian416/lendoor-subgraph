9aaa7f7650e49f6c5efe968a24834fd156b59d3a
fabiandiaz416 <diazfabian99@icloud.com>
2025-12-08 11:48:03 -0300

    feat: indexing in order to get price per share to get a 30 day apy

--- lendoor-sub/src/beacon-proxy.ts
+++ lendoor-sub/src/beacon-proxy.ts
@@ -1,14 +1,111 @@
-import { Genesis as GenesisEvent } from "../generated/BeaconProxy/BeaconProxy"
-import { Genesis } from "../generated/schema"
+import { Bytes, BigInt } from "@graphprotocol/graph-ts"
 
-export function handleGenesis(event: GenesisEvent): void {
-  let entity = new Genesis(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
+import {
+  Deposit as DepositEvent,
+  Withdraw as WithdrawEvent,
+  Borrow as BorrowEvent,
+  Repay as RepayEvent,
+  VaultStatus as VaultStatusEvent,
+} from "../generated/BeaconProxy/BeaconProxy"
 
+import {
+  VaultActivity,
+  VaultStatusSnapshot,
+} from "../generated/schema"
+
+function buildIdFromEvent(prefix: string, txHash: Bytes, logIndex: BigInt): string {
+  return prefix + "-" + txHash.toHex() + "-" + logIndex.toString()
+}
+
+// ---------------- ACTIVITY FEED ----------------
+
+export function handleDeposit(event: DepositEvent): void {
+  let id = buildIdFromEvent("deposit", event.transaction.hash, event.logIndex)
+  let entity = new VaultActivity(id)
+  entity.type = "DEPOSIT"
+  entity.account = event.params.owner
+  entity.counterparty = event.params.sender
+  entity.assets = event.params.assets
+  entity.shares = event.params.shares
+  entity.txHash = event.transaction.hash
+  entity.logIndex = event.logIndex
   entity.blockNumber = event.block.number
   entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.save()
+}
+
+export function handleWithdraw(event: WithdrawEvent): void {
+  let id = buildIdFromEvent("withdraw", event.transaction.hash, event.logIndex)
+  let entity = new VaultActivity(id)
+  entity.type = "WITHDRAW"
+  entity.account = event.params.owner
+  entity.counterparty = event.params.receiver
+  entity.assets = event.params.assets
+  entity.shares = event.params.shares
+  entity.txHash = event.transaction.hash
+  entity.logIndex = event.logIndex
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.save()
+}
+
+export function handleRepay(event: RepayEvent): void {
+  let id = buildIdFromEvent("repay", event.transaction.hash, event.logIndex)
+  let entity = new VaultActivity(id)
+
+  entity.type = "REPAY"
+  entity.account = event.params.account
+
+  entity.assets = event.params.assets
+  entity.unset("shares")
+  entity.unset("counterparty")
+
+  entity.txHash = event.transaction.hash
+  entity.logIndex = event.logIndex
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
 
   entity.save()
+}
+
+export function handleBorrow(event: BorrowEvent): void {
+  let id = buildIdFromEvent("borrow", event.transaction.hash, event.logIndex)
+  let entity = new VaultActivity(id)
+
+  entity.type = "BORROW"
+  entity.account = event.params.account
+
+  entity.assets = event.params.assets
+  entity.unset("shares")
+  entity.unset("counterparty")
+
+  entity.txHash = event.transaction.hash
+  entity.logIndex = event.logIndex
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+
+  entity.save()
+}
+
+// ---------------- STATUS SNAPSHOTS ----------------
+
+export function handleVaultStatus(event: VaultStatusEvent): void {
+  let id = buildIdFromEvent("status", event.transaction.hash, event.logIndex)
+  let snapshot = new VaultStatusSnapshot(id)
+
+  snapshot.totalShares = event.params.totalShares
+  snapshot.totalBorrows = event.params.totalBorrows
+  snapshot.accumulatedFees = event.params.accumulatedFees
+  snapshot.cash = event.params.cash
+  snapshot.interestAccumulator = event.params.interestAccumulator
+  snapshot.interestRate = event.params.interestRate
+
+  snapshot.vaultTimestamp = event.params.timestamp
+
+  snapshot.blockNumber = event.block.number
+  snapshot.blockTimestamp = event.block.timestamp
+  snapshot.txHash = event.transaction.hash
+  snapshot.logIndex = event.logIndex
+
+  snapshot.save()
 }
